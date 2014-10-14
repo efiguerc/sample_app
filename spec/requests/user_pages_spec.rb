5084a3fdd54fc599315d1e2e401c8bf3f2d3e1eb
Edgar Figueroa <efiguerc@gmail.com>
2014-10-13 23:13:28 -0500

    Finished 7.6 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,6 +17,94 @@ describe "User pages" do
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+
+    describe "filled with" do
+
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      let(:submit) { "Create my account" }
+
+      describe "blank Name" do
+        before do
+          fill_in "Name",         with: ""
+          click_button submit
+        end
+
+        it{ should have_content("Name can't be blank") }
+      end
+
+      describe "blank Email" do
+        before do
+          fill_in "Email",        with: ""
+          click_button submit
+        end
+
+        it{ should have_content("Email can't be blank") }
+      end
+
+      describe "blank Password" do
+        before do
+          fill_in "Password",     with: ""
+          fill_in "Confirmation", with: ""
+          click_button submit
+        end
+
+        it{ should have_content("Password can't be blank") }
+      end
+
+      describe "short Password" do
+        before do
+          fill_in "Password",     with: "aaa"
+          fill_in "Confirmation", with: "aaa"
+          click_button submit
+        end
+
+        it{ should have_content("Password is too short") }
+      end
+
+      describe "invalid Email" do
+        it "display an error" do
+          fill_in "Email", with: 'user@foo,com'
+          click_button submit
+          should have_content("Email is invalid")
+        end
+
+        it "display an error" do
+          fill_in "Email", with: 'user_at_foo.org'
+          click_button submit
+          should have_content("Email is invalid")
+        end
+
+        it "display an error" do
+          fill_in "Email", with: 'example.user@foo.'
+          click_button submit
+          should have_content("Email is invalid")
+        end
+
+        it "display an error" do
+          fill_in "Email", with: 'foo@bar_baz.com'
+          click_button submit
+          should have_content("Email is invalid")
+        end
+
+        it "display an error" do
+          fill_in "Email", with: 'foo@bar+baz.com'
+          click_button submit
+          should have_content("Email is invalid")
+        end
+
+        it "display an error" do
+          fill_in "Email", with: 'foo@bar..com'
+          click_button submit
+          should have_content("Email is invalid")
+        end
+      end
+    end
   end
 
   describe "signup" do
@@ -29,6 +117,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end  
     end
 
     describe "with valid information" do
@@ -41,6 +136,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
