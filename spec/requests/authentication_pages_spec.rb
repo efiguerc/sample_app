281d42e8aefd2fe8f152bc6ec80f9a3875190d6d
Edgar Figueroa <efiguerc@gmail.com>
2014-10-26 12:19:32 -0600

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,6 +48,18 @@ RSpec.describe "AuthenticationPages", :type => :request do
 		    it { should_not have_link('Profile',     href: user_path(user)) }
 		    it { should_not have_link('Settings',    href: edit_user_path(user)) }
 
+		    describe "in the Relationships controller" do
+	        describe "submitting to the create action" do
+	          before { post relationships_path }
+	          specify { expect(response).to redirect_to(signin_path) }
+	        end
+
+	        describe "submitting to the destroy action" do
+	          before { delete relationship_path(1) }
+	          specify { expect(response).to redirect_to(signin_path) }
+	        end
+	      end
+
 		    describe "when attempting to visit a protected page" do
 		      before do
 		        visit edit_user_path(user)
@@ -58,7 +70,7 @@ RSpec.describe "AuthenticationPages", :type => :request do
 
 		      describe "after signing in" do
 	          
-	          it "should render the desired protected page" do
+	          	it "should render the desired protected page" do
 		          expect(page).to have_title('Edit user')
 		        end
 
@@ -94,6 +106,16 @@ RSpec.describe "AuthenticationPages", :type => :request do
 		        before { visit users_path }
 		        it { should have_title('Sign in') }
 		      end
+
+		      describe "visiting the following page" do
+	          before { visit following_user_path(user) }
+	          it { should have_title('Sign in') }
+	        end
+
+	        describe "visiting the followers page" do
+	          before { visit followers_user_path(user) }
+	          it { should have_title('Sign in') }
+	        end
 		    end
 
 		    describe "in the Microposts controller" do
